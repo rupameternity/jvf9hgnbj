abd53faf1693681389e6cbca7b66f5e87c70684f
rupameternity <116637140+rupameternity@users.noreply.github.com>
2026-01-25 23:11:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ const ownerId = 7096965198;
 // Replace these numbers with your real Group IDs.
 // You can add more than 2 if you want.
 const allowedGroups = [
-    -2562222456, 
-    -2693902733
+    -1002562222456, 
+    -1002693902733
 ];
 
 // --- VARIABLES ---
